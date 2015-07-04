224bfd91b23a7496e8f1a7deab4d9790cbbf33d5
Matt Jones <mattjones@yieldbot.com>
2015-07-04 11:54:19 -0400

    deploy

--- lib/sensu-plugins-sms.rb
+++ lib/sensu-plugins-sms.rb
@@ -1,15 +1,1 @@
-
 require 'sensu-plugins-sms/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsSMS
-  class << self
-  end
-
-  class << self
-  end
-end
--- lib/sensu-plugins-sms/version.rb
+++ lib/sensu-plugins-sms/version.rb
@@ -1,6 +1,3 @@
-require 'json'
-
-# encoding: utf-8
 module SensuPluginsSMS
   # This defines the version of the gem
   module Version
@@ -9,20 +6,5 @@ module SensuPluginsSMS
     PATCH = 1
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
-
-    NAME   = 'sensu-plugins-sms'
-    BANNER = "#{NAME} v%s"
-
-    module_function
-
-    def version
-      format(BANNER, VER_STRING)
-    end
-
-    def json_version
-      {
-        'version' => VER_STRING
-      }.to_json
-    end
   end
 end
